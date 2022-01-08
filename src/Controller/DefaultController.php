d062e632147b91f1d87caed623456775fe02a7a3
PikkMoune71 <antoinepicard385@mail.com>
2022-01-08 22:49:25 +0100

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,20 @@
 
 namespace App\Controller;
 
-class DefaultController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/", name="app_homepage")
+     * @return Response
+     */
+    public function homepage(): Response
+    {
+        return $this->render("Frontend/home.html.twig");
+    }
+
 
 }
